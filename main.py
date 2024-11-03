4cd14699f2291c05444835b643957bac4880713a
EvilSquirrelGuy <evil.squirrelo.xtreme@gmail.com>
2024-11-03 15:21:12 +0000

    add tagging and better video gen logic

--- main.py
+++ main.py
@@ -5,6 +5,10 @@
 from colorama import Fore, Back, Style
 from collections import deque
 from itertools import islice
+from mutagen.mp4 import MP4, MP4Cover
+from datetime import datetime
+from PIL import Image
+from io import BytesIO
 
 import subprocess
 import ffmpeg
@@ -15,7 +19,7 @@ import shutil
 import librosa
 
 AUDIO_FILE_EXTENSIONS = (".mp3", ".m4a", ".ogg", ".flac", ".wav", ".opus")
-IMAGE_FILE_EXTENSIONS = (".png", ".jpg", ".jpeg", ".tiff", ".tif", ".bmp")
+IMAGE_FILE_EXTENSIONS = (".png", ".jpg", ".jpeg", ".tif", ".tiff", ".bmp")
 
 # setup encoders
 if sys.platform == "darwin":
@@ -80,11 +84,15 @@ def main() -> None:
   print(Style.RESET_ALL)
   print(f"{Style.BRIGHT}{Fore.GREEN}How to use:{Style.RESET_ALL}")
   print(f"{Fore.GREEN}1. Put all your desired sounds into the `sounds/` folder{Style.DIM}")
-  print(f"   | This script will see the following filetypes: .mp3, .m4a, .ogg, .flac, .wav")
+  print(f"   | This script will see the following filetypes: {', '.join(AUDIO_FILE_EXTENSIONS)}")
   print(f"   | They will be converted to opus 160k internally.")
-  print(f"{Style.NORMAL}2. Let the script guide you through the process.")
-  print(f"3. Select you preferred export format.")
-  print(f"4. Share your video for maximum annoyance!")
+  print(f"2. Place the image you would like to use for the cover art and video into the `images` directory.{Style.DIM}")
+  print(f"   | You will then be prompted to select an image in the folder and the script will generate")
+  print(f"   | a video and audio track for you! Make sure your image has one of the following file extensions:")
+  print(f"   | {', '.join(IMAGE_FILE_EXTENSIONS)}")
+  print(f"{Style.NORMAL}3. Let the script guide you through the process.")
+  print(f"4. Select you preferred export format.")
+  print(f"5. Share your video for maximum annoyance!")
   print(f"{Style.DIM}   | If you upload to video sharing sites, please share a link to the repository")
   print(f"   | so others can find this project too!")
   print(Style.RESET_ALL)
@@ -109,18 +117,30 @@ def main() -> None:
   processed_files = []
   durations = []
 
+  images = [os.path.join("images", img) for img in os.listdir("images")]
+
+  print(f"{Style.BRIGHT}Found the following images files:{Style.RESET_ALL}{Style.DIM}\n")
+
+  for i, img_path in enumerate(images):
+    print(f"  {i+1: 2}) {img_path}")
+  print()
+
+  img_num = int(input(f"{Style.RESET_ALL}{Fore.GREEN}{Style.BRIGHT}Select image from list: {Style.RESET_ALL}"))
+  print()
+
+  img = images[img_num-1]
+
   for file in audio_files:
     print(f"{Fore.BLUE}{Style.DIM}==>  {Style.NORMAL}Transcoding `{file}` to ogg/opus.")
     processed_files.append(transcode_to_opus(file))
     durations.append(librosa.get_duration(path=file))
-
 
   num_sounds = int(input(f"{Style.BRIGHT}{Fore.GREEN}Enter number of sounds to add in total (recommended 15-60 per 1h): {Style.RESET_ALL}"))
   duration = input(f"{Style.BRIGHT}{Fore.GREEN}Enter output duration [3600s]: {Style.RESET_ALL}")
   if duration == "":
     duration = 3600
   else:
-    duration = float(duration)
+    duration = float(duration.rstrip("s"))
   
   # generate indices for the sounds
   indices = list(islice(gap_random_uniform(0, duration-max(durations), max(durations)+1), num_sounds))
@@ -148,52 +168,111 @@ def main() -> None:
 
   # concatenate the files
   print(f"{Fore.YELLOW}{Style.NORMAL}==>  {Style.BRIGHT}Concatenating {len(result_files)} files...")
+
+
   stream = ffmpeg.concat(*[ffmpeg.input(rf) for rf in result_files], a=1, n=2, v=0)
+
   file_id = [chr(random.choice([0x61, 0x41])+random.randint(0,25)) for _ in range(6)]
   outfile = f"{num_sounds}_{int(duration)}s_silence_{''.join(file_id)}"
+
   stream = stream.output(f"{outfile}.m4a", acodec=AUDIO_ENC, audio_bitrate=192_000)
+
   stream.run(quiet=True)
-  print(f"{Fore.YELLOW}{Style.NORMAL}==>  {Style.BRIGHT}Successfully created file: `{outfile}.m4a`\n")
+  print(f"{Fore.YELLOW}{Style.NORMAL}==>  {Style.BRIGHT}Successfully concatenated audio: `{outfile}.m4a`\n")
+
+  print(f"{Fore.YELLOW}{Style.NORMAL}==>  {Style.BRIGHT}Writing audio metadata...")
+
+  audio_file = MP4(f"{outfile}.m4a")
+
+  # tag the file
+
+  audio_file["\xa9gen"] = "Shitpost"
+  audio_file["\xa9cmt"] = "Generated using BrokenSilence"
+  audio_file["\xa9day"] = str(datetime.now().strftime("%Y-%m-%d"))
+  audio_file["\xa9ART"] = "EvilSquirrelGuy"
+  audio_file["\xa9wrt"] = "EvilSquirrelGuy"
+  audio_file["purl"] = "https://www.github.com/EvilSquirrelGuy/BrokenSilence"
+
+  audio_file["\xa9lyr"] = ', '.join([af.lstrip("sounds")[1:] for af in audio_files])
+
+  # convert and crop the image
+  thumb = Image.open(img)
+  thumb_path = os.path.join(".tmp", "thumbnail")
+
+  (w, h) = thumb.size
+
+  new_size = min(w, h)
+
+  left = (w-new_size)/2
+  top = (h-new_size)/2
+  right = (w+new_size)/2
+  bottom = (h+new_size)/2
+
+  thumb = thumb.crop((left, top, right, bottom))
+  thumb.thumbnail((1024, 1024))
+
+  thumb.save(thumb_path, "PNG")
+
+  with open(thumb_path, "rb") as thumb_bin:
+    audio_file["covr"] = [
+      MP4Cover(thumb_bin.read(), imageformat=MP4Cover.FORMAT_PNG)
+    ]
+  
+  audio_file.save()
 
   print(f"{Fore.BLUE}{Style.DIM}==>  {Style.NORMAL}Cleaning temporary files...")
   shutil.rmtree(".tmp")
+  # os.remove(f"{outfile}.na.m4a")
 
   print(f"\n\n{Style.BRIGHT}{Fore.GREEN}Time to generate a video!{Style.RESET_ALL}")
-  print(f"{Fore.GREEN}Before we begin, please place the image you would like to use into the `images` directory.")
-  print(f"You will then be prompted to select an image in the folder and the script will generate a vide for you!")
-  print(f"{Fore.RED}{Style.BRIGHT}WARNING: {Style.RESET_ALL}{Fore.RED}This process will take a long time (10-20 minutes for a 1h video), so go grab a cup of tea while you wait.\n{Style.RESET_ALL}")
+  print(f"{Fore.RED}{Style.BRIGHT}WARNING: {Style.RESET_ALL}{Fore.RED}This process may take quite long depending on your system and the video duration. Also the resulting filesize may be quite large (~200MB/1hr).\n{Style.RESET_ALL}")
 
   input(f"\n{Style.DIM}Press Enter to continue...{Style.RESET_ALL}")
   print()
 
-  images = [os.path.join("images", img) for img in os.listdir("images")]
-
-  for i, img in enumerate(images):
-    print(f"  {i+1: 2}) {img}")
-  print()
-
-  vid_img = int(input(f"{Style.BRIGHT}{Fore.GREEN}Select image from list: {Style.RESET_ALL}"))
-  print()
-
-  img = images[vid_img-1]
-
   print(f"{Fore.YELLOW}{Style.NORMAL}==>  {Style.BRIGHT}Generating code for video...")
 
-  img_stream = ffmpeg.input(img, loop=1)
+  img_stream = ffmpeg.input(img, loop=1, framerate=1)
   audio_stream = ffmpeg.input(f"{outfile}.m4a")
-
 
   img_stream = ffmpeg.filter(img_stream, "scale", "1920", "1080", force_original_aspect_ratio="decrease")
   img_stream = ffmpeg.filter(img_stream, "pad", "1920", "1080", "-1", "-1")
   
-  vid_stream = ffmpeg.output(img_stream, audio_stream, f"{outfile}.mp4", acodec="copy", vcodec=VIDEO_ENC, pix_fmt="yuv420p", shortest=None)
+  vid_stream = ffmpeg.output(img_stream, audio_stream.audio, f"{outfile}.mp4", acodec="copy", vcodec=VIDEO_ENC, pix_fmt="yuv420p", shortest=None, tune="stillimage", preset="fast")
 
 
   vid_stream.run()
   print(f"{Fore.YELLOW}{Style.NORMAL}==>  {Style.BRIGHT}Successfully created file: `{outfile}.mp4`\n")
+  print(f"{Fore.YELLOW}{Style.NORMAL}==>  {Style.BRIGHT}Writing video metadata...")
 
+  video_file = MP4(f"{outfile}.mp4")
 
-  # ffmpeg -loop 1 -i deepfried_zhongli.png -i 32_3600s_silence_FoiBzV.ogg -c:v hevc_videotoolbox -tune stillimage -vf "scale=1920:1080:force_original_aspect_ratio=decrease,pad=1920:1080:-1:-1" -c:a aac_at -b:a 192k -pix_fmt yuv420p -shortest 1hr_silence_with_osmanthus_wine.mov
+  video_file["\xa9gen"] = "Shitpost"
+  video_file["\xa9cmt"] = "Generated using BrokenSilence"
+  video_file["desc"] = ("Generated using BrokenSilence\n"
+    "https://www.github.com/EvilSquirrelGuy/BrokenSilence\n\n"
+    f"Duration: {duration} seconds\n"
+    f"No. of sounds: {num_sounds}\n"
+    "Sound files used:\n- "
+    '\n- '.join([af.lstrip("sounds")[1:] for af in audio_files])
+    )
+  video_file["\xa9day"] = str(datetime.now().strftime("%Y-%m-%d"))
+  video_file["\xa9ART"] = "EvilSquirrelGuy"
+
+  cov_bfr = BytesIO()
+  cov_img = Image.open(img)
+  cov_img.save(cov_bfr, "PNG")
+  cov_bfr.name = "cover.png"
+  cov_bfr.seek(0)
+
+  video_file["covr"] = [
+    MP4Cover(cov_bfr.read(), imageformat=MP4Cover.FORMAT_PNG)
+  ]
+
+  video_file.save()
+
+  print(f"{Fore.YELLOW}{Style.NORMAL}==>  {Style.BRIGHT}Finished creating video.")
+
 
 
 if __name__ == "__main__":
